9b667657f8cb4fb40d460a0f68dc464cb3ee109a
UmairPervaiz <31058192+UmairPervaiz@users.noreply.github.com>
2021-12-16 18:26:13 +0500

    updated punch table and removed foreign key constraints

--- database/migrations/2021_09_24_113748_create_schedule_types_table.php
+++ database/migrations/2021_09_24_113748_create_schedule_types_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -22,29 +23,29 @@ class CreateScheduleTypesTable extends Migration
         });
 
         DB::table('schedule_types')->insert([
-            'name' => 'regular',
-            'created_at' => \Carbon\Carbon::now(),
-            'updated_at' => \Carbon\Carbon::now(),
+            'schedule_name' => 'regular',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
         DB::table('schedule_types')->insert([
-            'name' => 'morning shift',
-            'created_at' => \Carbon\Carbon::now(),
-            'updated_at' => \Carbon\Carbon::now(),
+            'schedule_name' => 'morning shift',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
         DB::table('schedule_types')->insert([
-            'name' => 'evening shift',
-            'created_at' => \Carbon\Carbon::now(),
-            'updated_at' => \Carbon\Carbon::now(),
+            'schedule_name' => 'evening shift',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
         DB::table('schedule_types')->insert([
-            'name' => 'night shift',
-            'created_at' => \Carbon\Carbon::now(),
-            'updated_at' => \Carbon\Carbon::now(),
+            'schedule_name' => 'night shift',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
         DB::table('schedule_types')->insert([
-            'name' => 'special shift',
-            'created_at' => \Carbon\Carbon::now(),
-            'updated_at' => \Carbon\Carbon::now(),
+            'schedule_name' => 'special shift',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
 
     }
