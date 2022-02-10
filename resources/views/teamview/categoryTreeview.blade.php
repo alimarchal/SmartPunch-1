74697e6033bec786faea476788807b8e009c766b
Ali Raza Marchal <alirazamarchal@hotmail.com>
2022-02-10 21:37:23 +0500

    teamview

--- resources/views/teamview/categoryTreeview.blade.php
+++ resources/views/teamview/categoryTreeview.blade.php
@@ -1,7 +1,14 @@
-<ul class="tree">
+<ul class="tree" >
     @foreach($parentCategories as $category)
             <li>
-                {{$category->name}}
+                <a href="#" style="color: blue; "> {{$category->name}}
+                     - <span style="color: black;">Designation:</span>
+                    <span style="color: red;">{{$category->designation}}</span>
+                    @if(!empty($category->office))
+                        - <span style="color: green;">Office: {{$category->office->name}}</span>
+                    @endif
+
+                    </a>
         @if(count($category->subcategory))
             @include('teamview.subCategoryList',['subcategories' => $category->subcategory])
         @endif
--- resources/views/teamview/subCategoryList.blade.php
+++ resources/views/teamview/subCategoryList.blade.php
@@ -1,6 +1,13 @@
 @foreach($subcategories as $subcategory)
     <ul>
-        <li>{{$subcategory->name}}</li>
+        <li><a href="#"  style="color: blue; ">{{$subcategory->name}}
+                - <span style="color: black;">Designation:</span>
+                <span style="color: red;">{{$subcategory->designation}} </span>
+                @if(!empty($category->office))
+                    - <span style="color: green;">Office: {{$subcategory->office->name}}</span>
+                @endif
+            </a>
+        </li>
         @if(count($subcategory->subcategory))
             @include('teamview.subCategoryList',['subcategories' => $subcategory->subcategory])
         @endif
