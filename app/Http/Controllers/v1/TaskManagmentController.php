1735131af2f2786e7bdb637b767d2ab62ecaaa66
Ali Raza Marchal <alirazamarchal@hotmail.com>
2022-06-30 13:59:39 +0500

    api

--- app/Http/Controllers/v1/TaskManagmentController.php
+++ app/Http/Controllers/v1/TaskManagmentController.php
@@ -3,9 +3,35 @@
 namespace App\Http\Controllers\v1;
 
 use App\Http\Controllers\Controller;
+use App\Models\TaskManagment;
 use Illuminate\Http\Request;
 
 class TaskManagmentController extends Controller
 {
     //
+
+    public function index(Request $request)
+    {
+        $task_management = TaskManagment::paginate(10);
+        return response()->json($task_management, 200);
+    }
+
+
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'task_name' => 'required',
+            'task_progress' => 'required',
+            'business_id' => 'required',
+            'office_id' => 'required',
+            'start_date' => 'required',
+            'end_date' => 'required',
+            'assign_to' => 'required',
+            'assign_from' => 'required',
+            'comment' => 'required',
+        ]);
+        $task_management = TaskManagment::create($request->all());
+        return response()->json($task_management, 200);
+    }
 }
--- app/Models/TaskManagment.php
+++ app/Models/TaskManagment.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class TaskManagment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'task_name',
+        'task_progress',
+        'business_id',
+        'office_id',
+        'start_date',
+        'end_date',
+        'assign_to',
+        'assign_from',
+        'comment',
+    ];
 }
